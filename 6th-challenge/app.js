cdfca7c25e452f6fdc2489c9376d408ffcca9b7c
André 'Dezzy <andrevcv1@gmail.com>
2019-11-05 23:22:48 +0000

    Refactor 6th challenge :recycle:

--- 6th-challenge/app.js
+++ 6th-challenge/app.js
@@ -1,28 +1,6 @@
 const YEAR = 1985;
 
 function run() {
-    // The number 1985 is on the line 45
-    // The line 45 has 88 numbers, starts at 1937 until 2025
-
-    const _printSequences = () => {
-        const LIMIT = 45;
-
-        let printCount = 1;
-        let count = 1;
-
-        for (let i = 0; i < LIMIT; i++) {
-            let numbers = "";
-
-            for (let j = 0; j < printCount; j++) {
-                numbers += `${count++} `;
-            }
-
-            printCount += 2;
-
-            console.log(`Line ${i + 1} -> ${numbers}`);
-        }
-    };
-
     const _getLineOf = (n) => {
         let squareRoot = Math.sqrt(n);
 
