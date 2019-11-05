1940da8d5f24d509c3b3a43cc44fbe1e044f67fd
André 'Dezzy <andrevcv1@gmail.com>
2019-11-05 00:35:01 +0000

    Initial commit :tada:

---
+++ 1st-challenge/app.js
@@ -0,0 +1,23 @@
+const START_AT = 1;
+const LIMIT = 1985;
+
+function run() {
+    var pairSum = 0;
+    var oddSum = 0;
+
+    for (let i = START_AT; i <= LIMIT; i++) {
+        console.log(i);
+
+        if (i % 2 == 0) {
+            pairSum += i;
+        } else {
+            oddSum += i;
+        }
+    }
+
+    const difference = oddSum - pairSum;
+
+    console.log(`The difference between the sum of odd numbers and sum of pair numbers of ${START_AT} until ${LIMIT}, is: ${difference}.`)
+}
+
+run();
