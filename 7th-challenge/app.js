15969af22d95365c975ff0e3604dd53294f93508
André 'Dezzy <andrevcv1@gmail.com>
2019-11-06 00:33:40 +0000

    Finish 7th challenge :sparkles:

--- 7th-challenge/app.js
+++ 7th-challenge/app.js
@@ -6,10 +6,30 @@ const JWT_TOKEN =
     "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkYzAxOTA3NTY2NTUzMGRmZDViNzMxMSIsImlhdCI6MTU3Mjk5MjExMywiZXhwIjoxNTczMDc4NTEzfQ.qfb1P8mfsLbW5Lj32MTARBm-52y2eUUK_P-8F6yPGTE";
 
 function run() {
+    const _sortByDate = (arr, dateField) => {
+        return arr.sort((a, b) => {
+            return new Date(a[dateField]) - new Date(b[dateField]);
+        })
+    }
+
+    const _findIndexWhere = (arr, field, value) => {
+        for (let index in arr) {
+            const obj = arr[index];
+
+            if(obj[field] === value) return index;
+        }
+    }
+
     fetch(API_URL, { headers: { Authorization: JWT_TOKEN } })
         .then(response => response.json())
         .then(json => {
-            console.log(json)
+            const transmissions = json.transmissoes;
+
+            const sortedTransmissions = _sortByDate(transmissions, 'data');
+            const indexWhere = _findIndexWhere(sortedTransmissions, 'codigo', 'zfIymAbIbI');
+
+            console.log(sortedTransmissions);
+            console.log(indexWhere);
         });
 }
 
